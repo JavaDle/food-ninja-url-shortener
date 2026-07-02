5e68af1582022a929923f72d8e27daa67fefc6ea
Husein JavaDLE <huseynjavadle@gmail.com>
2026-07-02 04:53:57 +0400

    Update LinkStatsOverview.php

--- app/Filament/Widgets/LinkStatsOverview.php
+++ app/Filament/Widgets/LinkStatsOverview.php
@@ -54,12 +54,10 @@ class LinkStatsOverview extends StatsOverviewWidget
         ];
 
         if (auth()->user()->isAdmin()) {
-            array_merge($stats, [
-                Stat::make(trans('admin.users'), number_format(User::query()->count()))
-                    ->description(trans('admin.registered_in_system'))
-                    ->descriptionIcon(Heroicon::Users)
-                    ->color('gray'),
-            ]);
+            $stats[] = Stat::make(trans('admin.users'), number_format(User::query()->count()))
+                ->description(trans('admin.registered_in_system'))
+                ->descriptionIcon(Heroicon::Users)
+                ->color('gray');
         }
 
         return $stats;
